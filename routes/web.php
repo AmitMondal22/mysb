4169b86c1a32ac42f2b4a11791c263c875a61aba
meettan <meettan@synergicsoftek.com>
2023-12-13 19:27:14 +0530

    rdtgbrf

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+ Route::get('/', function () {
+     return view('welcome');
+ });
 
-Route::get('/',[Controller::class,'mainpage']);
+//Route::get('/',[Controller::class,'mainpage']);
 
 
 /*------------------------------------------
